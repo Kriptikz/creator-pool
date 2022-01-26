d0f924e752a7dbeffcad794e9d48a70d6f7645c5
Kriptikz <cruzxkriptikz@gmail.com>
2022-01-26 08:57:48 +0100

    updated program id

--- programs/creator-pool/src/lib.rs
+++ programs/creator-pool/src/lib.rs
@@ -1,6 +1,6 @@
 use anchor_lang::prelude::*;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("E3mHBkUKFf1hhXN6YEG3oXD5G8SZkvVfoRQ4TNQewqYJ");
 
 #[program]
 pub mod creator_pool {
