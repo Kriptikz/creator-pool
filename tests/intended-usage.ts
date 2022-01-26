b6c8d785ab89e618c456822be7c1eec54c625fbe
Kriptikz <cruzxkriptikz@gmail.com>
2022-01-26 09:26:45 +0100

    added a test file for intended usage testing both programs together

--- tests/intended-usage.ts
+++ tests/intended-usage.ts
@@ -1,20 +1,18 @@
 import * as anchor from '@project-serum/anchor';
 import { Program } from '@project-serum/anchor';
 import { CreatorPool } from '../target/types/creator_pool';
+import { BaseStaking } from '../target/types/base_staking';
 
-describe('creator-pool', () => {
+describe('intended-usage', () => {
 
   // Configure the client to use the local cluster.
   const provider = anchor.Provider.env();
   anchor.setProvider(provider);
 
   const creatorPoolProgram = anchor.workspace.CreatorPool as Program<CreatorPool>;
+  const baseStakingProgram = anchor.workspace.BaseStaking as Program<BaseStaking>;
 
 
   it('Is initialized!', async () => {
-    // Add your test here.
-    const tx = await creatorPoolProgram.rpc.initialize({});
-    console.log("Your transaction signature", tx);
-
   });
 });
